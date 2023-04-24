f65288fddd2de61c75f82860a21c6ba7afa0ca76
adrian <16654653+adaviloper@users.noreply.github.com>
2023-04-24 15:26:08 -0500

    added port to env

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Backend
 composer install
 ./vendor/bin/sail up -d
 cp .env.example .env
 ./vendor/bin/sail artisan key:generate
 
+# Frontend
 cd resources/js || exit
 cp .env.example .env
 npm install
